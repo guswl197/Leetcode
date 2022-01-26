6a897ab79230004a0f0d7130809392c87f510e6b
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-01-26 21:53:41 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (23.38%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -3,19 +3,14 @@ public:
     int subtractProductAndSum(int n) {
         int pro=1;
         int sum=0;
-        vector<int> arr; 
         
         while(1){
-            arr.push_back(n%10); 
+            sum+=n%10;
+            pro*=n%10;
             n/=10; 
             if(n==0){
                 break; 
             }
-        }
-        
-        for(int i=0; i<arr.size();i++){
-            sum+=arr[i]; 
-            pro*= arr[i]; 
         }
         
         return pro-sum; 
