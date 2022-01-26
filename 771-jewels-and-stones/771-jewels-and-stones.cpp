3d63f6cfd2570d7c1c3254482731dd0fcc50d694
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-01-26 20:38:50 +0900

    Time: 8 ms (5.89%), Space: 6.5 MB (24.19%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int ans=0; 
+        unordered_set<char> setJ(jewels.begin(),jewels.end()); 
         
-        for(int i=0; i<jewels.size();i++){
-            for(int j=0; j<stones.size();j++){
-                if(jewels[i]==stones[j]){
-                    ans++; 
-                }
+        for(char s : stones){
+            if(setJ.count(s)){
+                ans++; 
             }
         }
-        
         return ans; 
     }
 };
