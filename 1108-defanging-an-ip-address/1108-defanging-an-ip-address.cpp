d0321b3d93c1bd126cb5b1cec41878f8093efa41
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-01-26 15:41:17 +0900

    Time: 3 ms (20.89%), Space: 6.8 MB (5.01%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    string defangIPaddr(string address) {
-        string::size_type pos=0; 
-        string::size_type offset=0; 
-        
-        while((pos=address.find(".",offset))!=string::npos){
-            address.replace(address.begin()+pos, address.begin()+pos+1,"[.]");
-            offset=pos+3; 
-        }
-        return address; 
-    }
+   string defangIPaddr(string address) {
+  return regex_replace(address, regex("\\."), "[.]");
+}
 };
