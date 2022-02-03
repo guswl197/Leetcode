84ab4f1cb5f4ab7853297bcee9110baa76457edd
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-02-03 20:20:50 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (21.70%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-        
-        string ans,num;
-        stringstream stream;
-        stream.str(s);
-        vector<string> v; 
-        
-        while(stream >> num){
-            v.push_back(num); 
+        for(int i=0; i<s.size();++i){
+            if(s[i]==' '&&--k==0){
+                return s.substr(0,i); 
+            }
         }
         
-        for(int i=0; i<k-1;i++){
-            ans+=v[i];
-            ans+=" "; 
-        }
-        ans+=v[k-1]; 
-        
-        return ans; 
+        return s; 
     }
 };
