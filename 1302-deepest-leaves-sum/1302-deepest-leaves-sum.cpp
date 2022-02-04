db2af9477d990d1c958a27b9ce652fc278245bd6
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-02-04 14:54:05 +0900

    Time: 187 ms (21.45%), Space: 61.9 MB (28.30%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,9 +11,11 @@
  */
 class Solution {
 public:
-    int deepest=0, sum=0;
+    //int deepest=0, sum=0;
+    int sum=0,n; 
     int deepestLeavesSum(TreeNode* root,int depth=0) {
-    /*====dfs=====*/
+    /*====dfs===== 재귀*/
+        /*
         int num=1;
         if(!root){
            return 0; 
@@ -32,6 +34,28 @@ public:
         deepestLeavesSum(root->left, depth+1); 
         deepestLeavesSum(root->right, depth+1); 
 
+        return sum; 
+        */ 
+        
+        /*====bfs===== queue*/
+        queue<TreeNode*> q;
+        q.push(root); 
+        while(!q.empty()){
+            sum=0, n=size(q); 
+            
+            for(int i=0; i<n;i++){
+                auto top=q.front(); 
+                q.pop();
+                sum+=top->val;
+                if(top->left){
+                    q.push(top->left); 
+                }
+                if(top->right){
+                    q.push(top->right); 
+                }
+            }
+        }
+        
         return sum; 
     }
 };
