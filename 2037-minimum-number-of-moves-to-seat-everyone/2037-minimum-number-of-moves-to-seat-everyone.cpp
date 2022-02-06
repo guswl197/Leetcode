92f9e1aeaa182a16ee691215492d2fca9ed56c1e
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-02-06 12:35:10 +0900

    Time: 19 ms (25.10%), Space: 18.1 MB (43.04%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
         
-        int sum=0; 
-        
+        int ans=0;
         sort(seats.begin(),seats.end()); 
         sort(students.begin(),students.end()); 
         
-        for(int i=0; i<students.size();i++){
-            sum+=abs(students[i]-seats[i]); 
+        for(int i=0; i<students.size(); i++){ 
+            ans+= abs(students[i]-seats[i]); 
         }
-        return sum; 
+        
+        return ans; 
     }
 };
