8ad8843f63406b7cd227a5cfe53d0fc2c4622e98
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-02-07 20:15:07 +0900

    Time: 0 ms (100.00%), Space: 6.8 MB (55.98%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
-    string removeOuterParentheses(string s) {
-        vector<string> v(10000); 
-        string ans; 
-        int j=0; 
-        int cnt=0;
-         
-        for(int i=0; i<s.size();i++){
-            if(s[i]=='('){
-                cnt++; 
-                v[j]+=(s[i]); 
-            }
-            else{
-                cnt--; 
-                v[j]+=(s[i]); 
-            }
-            
-            if(cnt==0){
-               j++; 
-            }
+   string removeOuterParentheses(string S) {
+        string res;
+        int opened = 0;
+        for (char c : S) {
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
         }
-    
-        for(int i=0; i<j;i++){
-            v[i].erase(v[i].size()-1); 
-            v[i].erase(v[i].begin());  
-            ans+=v[i];  
-        }
-        
-        return ans; 
+        return res;
     }
 };
