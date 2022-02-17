f35b1ceb003413d92853fbbadc34957c857cf125
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-02-17 15:40:21 +0900

    Time: 35 ms (49.47%), Space: 14.9 MB (42.81%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=num.size()-1; i>=0; i--){
             if(num[i]%2!=0){
                 return num.substr(0,i+1); 
-                break;
             }
         }
         
