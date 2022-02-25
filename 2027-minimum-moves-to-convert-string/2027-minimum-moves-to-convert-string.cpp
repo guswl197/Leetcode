9e426800536c4ad873dd0061c8f4a39970ac8241
guswl197 <80441723+guswl197@users.noreply.github.com>
2022-02-25 14:32:27 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (39.17%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-   int minimumMoves(string s) {
-	int i = 0, n = s.length(), count = 0;
-	while (i < n) {
-		if (s[i] == 'O')  
-			i++;
-		else
-			count++, i += 3;  
-	}
-	return count;
-}
+    int minimumMoves(string s) {
+        int ans=0; 
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='X'){
+                for(int j=0; j<3; j++){
+                    if(i+j<s.size()){
+                     s[i+j]='O';
+                    }
+                }
+                ans++; 
+            }
+        }
+        
+        return ans;
+    }
 };
